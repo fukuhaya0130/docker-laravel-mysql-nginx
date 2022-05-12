cd3562d2f888d8c94454de2f1d3493532e961827
h.fukuda <h.fukuda@L-0004.local>
2022-05-12 11:26:32 +0900

    first commit

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SampleController;
+use App\Http\Controllers\TextController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/samples/index', [SampleController::class, 'index'])->name("samples.index");
+
+Route::get("/text", [TextController::class, "index"])->name("text.index");
+Route::get("/text/create", [TextController::class, "create"])->name("text.create");
+Route::post("text/store", [TextController::class, "store"])->name("text.store");
+Route::get("/text/{id}", [TextController::class, "show"])->name("text.show");
+Route::get("/text/{id}/edit", [TextController::class, "edit"])->name("text.edit");
+Route::post("/text/{id}", [TextController::class, "update"])->name("text.update");
+Route::get("/text/{id}/destroy", [TextController::class, "destroy"])->name("text.destroy");
